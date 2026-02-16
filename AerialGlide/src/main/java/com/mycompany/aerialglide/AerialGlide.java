2ed2fa0e181f4f7eb8105857c626c92123e9ae3b
arenastiago30-dev <arenastiago3.0@gmail.com>
2026-02-16 01:13:57 -0300

    Add files via upload

---
+++ AerialGlide/src/main/java/com/mycompany/aerialglide/AerialGlide.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aerialglide;
+
+/**
+ *
+ * @author Alumno
+ */
+public class AerialGlide {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
